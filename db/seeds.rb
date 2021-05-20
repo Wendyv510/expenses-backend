055e6d27964ff5423ed57bcb9cc9be9b08d7ff20
Wendy Harris <wendyv510@hotmail.com>
2021-05-20 11:24:46 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 account = Account.create(name: 'Checking', balance: 1000)
 
-transaction = Transaction.new(account_id: 1, amount: 10, kind: 'deposit', date: Date.today, description: 'Found$10')
+transaction = Transaction.create(account_id: 1, amount: 10, kind: 'deposit', date: Date.today, description: 'Found$10')
 
